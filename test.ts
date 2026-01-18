ba76b6911077a81d8f28c03c1867176df4cc1b27
tomos2005-source <tomos2005@gmail.com>
2026-01-19 07:31:50 +0900

    更新 pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,15 @@
 // テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+// 1. 標準ブロックでシリアルを初期化
+serial.redirect(SerialPin.P1, SerialPin.P2, BaudRate.BaudRate115200)
+
+// 2. 拡張ブロックを使用
+wifiBoard.connectWiFi("SSID", "PASS")
+
+basic.forever(function () {
+    if (wifiBoard.isConnected()) {
+        let temp = wifiBoard.getSensorData(wifiBoard.SensorType.T1)
+        serial.writeLine("Log: " + temp) // 標準のシリアル出力も併用可能
+        wifiBoard.sendTB()
+    }
+    basic.pause(5000)
+})
